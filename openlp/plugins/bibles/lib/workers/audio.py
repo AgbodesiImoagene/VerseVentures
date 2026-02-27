d4613bc926aac33c60e3f773993c94fd6dda97a8
Ucheyo <uchenna89@gmail.com>
2026-02-27 22:40:26 +0000

    using sound device and not pyaudio, also using correct credential resolver method for AWS transcribe

--- openlp/plugins/bibles/lib/workers/audio.py
+++ openlp/plugins/bibles/lib/workers/audio.py
@@ -1,24 +1,3 @@
-# -*- coding: utf-8 -*-
-
-##########################################################################
-# OpenLP - Open Source Lyrics Projection                                 #
-# ---------------------------------------------------------------------- #
-# Copyright (c) 2008-2024 OpenLP Developers                              #
-# ---------------------------------------------------------------------- #
-# This program is free software: you can redistribute it and/or modify   #
-# it under the terms of the GNU General Public License as published by   #
-# the Free Software Foundation, either version 3 of the License, or      #
-# (at your option) any later version.                                    #
-#                                                                        #
-# This program is distributed in the hope that it will be useful,        #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of         #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
-# GNU General Public License for more details.                           #
-#                                                                        #
-# You should have received a copy of the GNU General Public License      #
-# along with this program.  If not, see <https://www.gnu.org/licenses/>. #
-##########################################################################
-
 from asyncio import new_event_loop
 import asyncio
 from datetime import datetime, timedelta
@@ -29,6 +8,7 @@ import numpy as np
 from pyaudio import PyAudio, paInt16
 from PyQt5 import QtCore
 from queue import Queue
+import sounddevice
 
 from amazon_transcribe.client import TranscribeStreamingClient
 from amazon_transcribe.handlers import TranscriptResultStreamHandler
@@ -37,21 +17,21 @@ from amazon_transcribe.model import (
     TranscriptEvent,
     TranscriptResultStream,
 )
+from amazon_transcribe.auth import Credentials, CredentialResolver, StaticCredentialResolver
 from speech_recognition import AudioData, Microphone, Recognizer
 
 from openlp.core.db.manager import DBManager
 from openlp.core.threading import ThreadWorker
 from openlp.plugins.bibles.lib.db import init_schema
 
+log = logging.getLogger(__name__)
 DEFAULT_SAMPLE_RATE = 16000  # 16 kHz
 BLOCK_SIZE = 4 * 1024  # 4 KB
 CHANNELS = 1
 SILENCE_DURATION = 2.5  # 2.5 seconds
 CALLBACK_INTERVAL = 1  # 1 second
 
-log = logging.getLogger(__name__)
 
-audio_queue = Queue()
 
 
 class AmazonStreamEventHandler(TranscriptResultStreamHandler):
@@ -83,11 +63,29 @@ class AmazonStreamEventHandler(TranscriptResultStreamHandler):
 
         :param transcript_event: The transcript event to handle.
         """
-        result = transcript_event.transcript.results[0]
-        transcription = result.alternatives[0].transcript
-        self.display_text.emit(transcription)
-        if not result.is_partial:
-            self.submitted_text.emit(transcription)
+
+        log.debug("AudioWorker - Entered handler transcript event func")
+
+        results = transcript_event.transcript.results
+        for result in results:
+            for alt in result.alternatives:
+                # log.info("AudioWorker - Received output from transcribe", alt.transcript)
+                print(alt.transcript)
+        
+        # log.debug("AudioWorker - Successfully received transcript event from AWS transcribe", transcript_event)
+
+
+
+        # log.debug("AudioWorker - Successfully received transcript event result from AWS transcribe", result)
+
+        
+        # transcription = result.alternatives[0].transcript
+
+        # log.debug("AudioWorker - Successfully received transcription", transcription)
+
+                self.display_text.emit(alt.transcript)
+                if not result.is_partial:
+                    self.submitted_text.emit(alt.transcript)
 
 
 class AudioWorker(ThreadWorker):
@@ -120,10 +118,15 @@ class AudioWorker(ThreadWorker):
         self.event_loop = None
         self.current_task = None
         self.stopper = None
-        # self.setup_microphone(None)
+        #self.setup_microphone(None)
+
+        # update access_key_id and secret_access_key with real AWS value credentials
+        self.static_credential_resolver = StaticCredentialResolver(access_key_id="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", secret_access_key="YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY")        
         self.client = TranscribeStreamingClient(
-            region=kwargs.get("region", "us-east-1")
+            region=kwargs.get("region", "us-east-1"),
+            credential_resolver = self.static_credential_resolver
         )
+
 
     def start(self):
         """
@@ -161,53 +164,63 @@ class AudioWorker(ThreadWorker):
 
         :param language_code: The language code for transcription.
         """
+        log.debug(
+            "AudioWorker - Running amazon transcribe"
+        )
         # Start transcription to generate our async stream
         amazon_stream = await self.client.start_stream_transcription(
             language_code=language_code,
             media_sample_rate_hz=DEFAULT_SAMPLE_RATE,
             media_encoding="pcm",
+
         )
 
+        log.debug(
+            "AudioWorker - Setup stream object for amazon transcribe"
+        )
         # Instantiate our handler and start processing events
         handler = AmazonStreamEventHandler(
             amazon_stream.output_stream, self.display_text, self.submitted_text
         )
+
+
+
+        log.debug(
+            "AudioWorker - Create handler for amazon transcribe"
+        )
         await asyncio.gather(self.write_chunks(amazon_stream), handler.handle_events())
 
-    def _start_listening(self):
+        log.debug(
+            "AudioWorker - Send stream message to amazon transcribe and awating result"
+        )
+
+    async def _start_listening(self):
         """
         Start listening to the microphone in the background.
         """
-        if self.microphone:
-            log.debug("AudioWorker - Listening in background")
-            self.stopper = self.recognizer.listen_in_background(
-                source=self.microphone,
-                callback=record_callback,
-                phrase_time_limit=CALLBACK_INTERVAL,
-            )
+        loop = asyncio.get_event_loop()
+        input_queue = asyncio.Queue()
 
-    def _stop_listening(self, wait=True):
-        """
-        Stop listening to the microphone in the background.
+        def callback(indata, frame_count, time_info, status):
+            loop.call_soon_threadsafe(input_queue.put_nowait, (bytes(indata), status))
 
-        :param wait: Whether to wait for the listener to stop.
-        """
-        if self.stopper:
-            log.debug("AudioWorker - Stopping background listening")
-            self.stopper(wait)
-            self.stopper = None
+        # Be sure to use the correct parameters for the audio stream that matches
+        # the audio formats described for the source language you'll be using:
+        # https://docs.aws.amazon.com/transcribe/latest/dg/streaming.html
+        stream = sounddevice.RawInputStream(
+            channels=1,
+            samplerate=16000,
+            callback=callback,
+            blocksize=1024 * 2,
+            dtype="int16",
+        )
+        # Initiate the audio stream and asynchronously yield the audio chunks
+        # as they become available.
+        with stream:
+            while True:
+                indata, status = await input_queue.get()
+                yield indata, status
 
-    def _adjust_for_ambient_noise(self):
-        """
-        Adjust the recognizer for ambient noise using the microphone.
-        """
-        if self.microphone:
-            log.debug("AudioWorker - Adjusting for ambient noise")
-            self._stop_listening()
-            with self.microphone as source:
-                self.recognizer.adjust_for_ambient_noise(source)
-            if self.is_active:
-                self._start_listening()
 
     async def write_chunks(self, amazon_stream: StartStreamTranscriptionEventStream):
         """
@@ -218,30 +231,21 @@ class AudioWorker(ThreadWorker):
         # This connects the raw audio chunks generator coming from the microphone
         # and passes them along to the transcription stream.
         try:
-            async for chunk in mic_stream():
+            async for chunk, status in self._start_listening():
                 if self.is_active:
                     await amazon_stream.input_stream.send_audio_event(audio_chunk=chunk)
+                    log.debug(
+            "AudioWorker - Mic stream has data and successfully sent data to amazon transcribe"
+                )
+
                 if self.shutdown:
                     break
         except asyncio.CancelledError:
             await amazon_stream.input_stream.end_stream()
+            log.debug(
+            "AudioWorker - Mic stream is empty. Nothing is being sent to amazon transcribe"
+                )
             raise
-
-    @QtCore.pyqtSlot(int)
-    def setup_microphone(self, microphone_source):
-        """
-        Set up the microphone for audio input.
-
-        :param microphone_source: The index of the microphone source.
-        """
-        log.debug("AudioWorker - Setup microphone %s", microphone_source)
-        self._stop_listening()
-        self.microphone = (
-            Microphone(sample_rate=DEFAULT_SAMPLE_RATE, device_index=microphone_source)
-            if microphone_source
-            else Microphone()
-        )
-        self._adjust_for_ambient_noise()
 
     @QtCore.pyqtSlot(bool)
     def toggle_active(self, state):
@@ -253,6 +257,7 @@ class AudioWorker(ThreadWorker):
         log.debug("AudioWorker - Toggle active %s", state)
         self.is_active = state
         if self.is_active:
+            self.static_credential_resolver.get_credentials
             self._start_listening()
             self._start_transcription_task()
         else:
@@ -285,64 +290,3 @@ class AudioWorker(ThreadWorker):
             self.current_task.cancel()
         self.event_loop.call_soon_threadsafe(self.event_loop.stop)
         self.quit.emit()
-
-
-def record_callback(_, audio: AudioData):
-    """
-    Threaded callback function to receive audio data when recordings finish.
-
-    :param audio: An AudioData containing the recorded bytes.
-    """
-    # Grab the raw bytes and push it into the thread safe queue.
-    data = audio.get_raw_data()
-    audio_queue.put_nowait(data)
-
-
-async def mic_stream():
-    """
-    Asynchronous generator to yield audio data from the microphone.
-    """
-    while True:
-        data = audio_queue.get()
-        yield data
-
-
-def get_working_microphones():
-    """
-    Get a list of working microphones.
-
-    :return: A dictionary of working microphones with their indices and names.
-    """
-    pa = PyAudio()
-    working_microphones = {}
-    try:
-        for device_index in range(pa.get_device_count()):
-            device_info = pa.get_device_info_by_index(device_index)
-            device_name = device_info["name"]
-            if (
-                device_info["maxInputChannels"] == 0
-                or device_info["hostApi"] != 0
-                or device_info["defaultSampleRate"] == 0
-            ):
-                continue
-            try:
-                # read audio
-                pyaudio_stream = pa.open(
-                    input_device_index=device_index,
-                    channels=1,
-                    format=paInt16,
-                    rate=int(device_info["defaultSampleRate"]),
-                    input=True,
-                )
-                try:
-                    _ = pyaudio_stream.read(1024)
-                    if not pyaudio_stream.is_stopped():
-                        pyaudio_stream.stop_stream()
-                finally:
-                    pyaudio_stream.close()
-                working_microphones[device_index] = device_name
-            except Exception:
-                continue
-    finally:
-        pa.terminate()
-    return working_microphones
